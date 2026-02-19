ca72fa1f72f7818aaca9b6742c30da19b1cee05f
xinzhengwen-edu <xinzheng.wen723@students.cmcss.net>
2026-02-19 11:30:28 -0600

    Update task2.py
    completion

--- task2.py
+++ task2.py
@@ -1,1 +1,20 @@
+from data import price
+
+#TODO 1 Run this program
+#Let'S debug.
+#This time the problem is causing an error
+#TODO 2 Comment lines 18 & 19 to allow the program to run.
+
+#TODO 3 print price
+print(price)
+
+#TODO 4 Print the type
+print(type(price))
+
+#TODO 5 Convert to the correct data type and reassign to price.
+price = float(price)
+
+#TODO 6 Uncomment 18 & 19.
+total = price + 5
+print(total)
 
