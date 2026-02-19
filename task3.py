7363df25916037409e6b9cdbd061a04f331cece4
xinzhengwen-edu <xinzheng.wen723@students.cmcss.net>
2026-02-19 11:30:53 -0600

    Update task3.py
    completion

--- task3.py
+++ task3.py
@@ -1,1 +1,28 @@
+from data import a, b, c, d, e, f
+
+# Convert each variable to the most suitable data type
+# Hint: print and check type if unsure
+# Not all can or should be converted to a number
+
+# a
+print(f"A: {a}, Type: {type(a)}")
+a = float(a)
+
+# b
+print(f"B: {b}, Type: {type(b)}")
+
+# c
+print(f"C: {c}, Type: {type(c)}")
+c = int(c)
+
+# d
+print(f"D: {d}, Type: {type(d)}")
+d = int(d)
+
+# e
+print(f"E: {e}, Type: {type(e)}")
+
+# f
+print(f"F: {f}, Type: {type(f)}")
+f = float(f)
 
