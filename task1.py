ede6fe09b717b3b93fa58f66371b4df1abfb8e43
xinzhengwen-edu <xinzheng.wen723@students.cmcss.net>
2026-02-19 11:29:46 -0600

    Update task1.py
    completion

--- task1.py
+++ task1.py
@@ -1,1 +1,16 @@
+from data import num
 
+#TODO 1 Run this program
+#Let'S debug.
+
+#TODO 2 print num
+print(num)
+
+#TODO 3 Print the type
+print(type(num))
+
+#TODO 4 Convert to the correct data type and reassign to num.
+num = int(num)
+
+result = num * 10
+print(result)
